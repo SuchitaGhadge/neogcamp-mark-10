8aaec37c2f5ef852d04bd436cc6dc17175954a73
Suchita Ghadge <36835240+SuchitaGhadge@users.noreply.github.com>
2021-08-29 22:55:09 +0530

    mark10 complete

---
+++ index.js
@@ -0,0 +1,45 @@
+const billAmount = document.querySelector("#billAmount");
+const cashGiven = document.querySelector("#cashGiven")
+const checkButton = document.querySelector("#checkButton");
+const message = document.querySelector("#errorMessage");
+const noOfNotes = document.querySelectorAll('.no-Of-Notes')
+const returnCash = document.querySelector("#caption")
+
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function ValidateAmount(){
+    hideMessage();
+    // function to check valid amount
+    if(billAmount.value>0){
+        if(Number(cashGiven.value)>= Number(billAmount.value)){
+            const returnAmount = Number(cashGiven.value) - Number(billAmount.value);
+            returnCash.innerHTML = returnAmount
+            // console.log(Number.isInteger(returnAmount));
+            calculateReturn(returnAmount);
+
+        }else{
+            showMessage("Cash given should be equal or greater than bil amount")
+        }
+    }else{
+      showMessage("Bill amount should be greater than 0")
+    }
+})
+
+function hideMessage(){
+    message.style.display = "none"
+}
+
+function showMessage(msg){
+    message.style.display = "block";
+    message.style.color = "yellow";
+    message.innerHTML = msg;
+}
+
+function calculateReturn(returnAmount){
+    for(i=0; i<availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(returnAmount / availableNotes[i]);
+        returnAmount = returnAmount % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    };
+   
+}
